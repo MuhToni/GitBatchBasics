b554772c082eeb768ebd46cc7c71b76ed421de02
SysAdmin <cadettoni@gmail.com>
2022-08-27 10:40:10 -0400

    three lines

--- src/test/java/class01/gitBasics.java
+++ src/test/java/class01/gitBasics.java
@@ -2,7 +2,7 @@ package class01;
 
 public class gitBasics {
     public static void main(String[] args) {
-        System.out.println("Today is beautiful");
+        System.out.println("Today is not beautiful");
 
     }
 }
